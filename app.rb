74b86742a5b0bbe21007b2e8e51d78f186c62d8e
jordan <jordantmunoz@gmail.com>
2017-06-10 20:33:21 -0700

    update content

--- app.rb
+++ app.rb
@@ -18,37 +18,28 @@ require 'net/http'
 
 post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
-  # # query = params[:text].gsub(params[:trigger_word], '').strip
-
-  # # action, repo = message.split('_').map {|c| c.strip.downcase }
+  
   action, query = message.split('_').map {|c| c.strip.downcase }
-
-  # repo_url = "https://api.github.com/repos/#{repo}"
-  # giphy_url = "http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=dc6zaTOxFJmzC&limit=5"
-
+  respond_message action
+  respond_message query
   case action
     when 'trending'
+      respond_message "INSIDE TRENDING"
       giphy_url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=1"     
+      respond_message "#{giphy_url}"
       resp = HTTParty.get(giphy_url)
       resp = JSON.parse resp.body
-      resp["data"].each do |gif|
-        respond_message gif["url"]
-      end
-      # respond_message resp
+      resp["data"].each {|gif| respond_message gif["url"]}
+  
     when 'search'
       giphy_url = "http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=dc6zaTOxFJmzC&limit=1"
       resp = HTTParty.get(giphy_url)
       resp = JSON.parse resp.body
-      
-  #     # respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
-  #     # respond_message "#{resp['body']}"
       resp["data"].each do |img|
         respond_message img["url"]
-        respond_message img["images"]["fixed_height"]["width"]
-        respond_message img["images"]["fixed_height"]["height"]
+        # respond_message img["images"]["fixed_height"]["width"]
+        # respond_message img["images"]["fixed_height"]["height"]
       end 
-  # end
-  # respond_message params.inspect
   end
 end
 

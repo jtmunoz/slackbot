3c6d5b10986756c672c5142d38d3956ccacebbe5
jordan <jordantmunoz@gmail.com>
2017-06-10 19:52:41 -0700

    updates content

--- app.rb
+++ app.rb
@@ -27,26 +27,25 @@ post '/gateway' do
   # giphy_url = "http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=dc6zaTOxFJmzC&limit=5"
 
   case action
-      when 'trending'
-        giphy_url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC"
-        resp = HTTParty.get(giphy_url)
-        resp = JSON.parse(resp.body)
-        resp["data"].each do |gif|
-          respond_message gif["url"]
-        end
-  #   when 'search'
-  #     # resp = HTTParty.get(repo_url)
-  #     resp = HTTParty.get(giphy_url)
-
-  #     resp = JSON.parse resp.body
+    when 'trending'
+      giphy_url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=1"     
+      resp = HTTParty.get(giphy_url)
+      resp = JSON.parse(resp.body)
+      resp["data"].each do |gif|
+        respond_message(gif["url"])
+      end
+    when 'search'
+      giphy_url = "http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=dc6zaTOxFJmzC&limit=1"
+      resp = HTTParty.get(giphy_url)
+      resp = JSON.parse resp.body
       
   #     # respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
   #     # respond_message "#{resp['body']}"
-  #      result["data"].each do |img|
-  #       puts img["url"]
-  #       puts img["images"]["fixed_height"]["width"]
-  #       puts img["images"]["fixed_height"]["height"]
-  #     end 
+      resp["data"].each do |img|
+        respond_message img["url"]
+        respond_message img["images"]["fixed_height"]["width"]
+        respond_message img["images"]["fixed_height"]["height"]
+      end 
   # end
   # respond_message params.inspect
   end

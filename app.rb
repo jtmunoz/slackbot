423dc0ce3dd2566c1982d032b513a506bc90d37a
jordan <jordantmunoz@gmail.com>
2017-06-14 21:01:43 -0700

    finishes trending feature, cleans up code

--- app.rb
+++ app.rb
@@ -3,19 +3,6 @@ require 'httparty'
 require 'json'
 require 'net/http'
 
-
-# get '/' do
-#   url = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5"
-#   resp = Net::HTTP.get_response(URI.parse(url))
-#   buffer = resp.body
-#   result = JSON.parse(buffer) 
-#   result["data"].each do |img|
-#     puts img["url"]
-#     puts img["images"]["fixed_height"]["width"]
-#     puts img["images"]["fixed_height"]["height"]
-#   end
-# end
-
 get '/' do
   redirect '/gateway' 
 end
@@ -26,35 +13,23 @@ end
 
 post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
-  # # query = params[:text].gsub(params[:trigger_word], '').strip
-
-  # # action, repo = message.split('_').map {|c| c.strip.downcase }
   action, query = message.split('_').map {|c| c.strip.downcase }
-
-  # repo_url = "https://api.github.com/repos/#{repo}"
   # giphy_url = "http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=dc6zaTOxFJmzC&limit=5"
-  # giphy_url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC"
 
   case action
-      when 'trending'
-        giphy_url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=2"
-        
-        resp = HTTParty.get(giphy_url)
-    
+    when 'trending'
+      giphy_url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=2"
+      resp = HTTParty.get(giphy_url)
+      buffer = resp.body
+      result = JSON.parse(buffer)        
+      respond_message result["data"][0]["url"]  
+  end
+end
 
-        buffer = resp.body
-          # puts "buffer" * 10
-          
-
-        result = JSON.parse(buffer) 
-          # puts "results" * 10
-          # puts result
-          # puts result.class
-
-       
-        respond_message result["data"][0]["url"]
-        
-
+def respond_message message
+  content_type :json
+  {:text => message}.to_json
+end
         # result["data"].each do |gif|
         #     puts gif["url"] 
         #     message << gif["url"]
@@ -65,8 +40,6 @@ post '/gateway' do
 
         
         # puts result["data"].length
-        
-  end
   #   when 'search'
   #     # resp = HTTParty.get(repo_url)
   #     resp = HTTParty.get(giphy_url)
@@ -83,9 +56,4 @@ post '/gateway' do
   # end
   # message = "#{params.inspect} #{action} #{query}"
   # respond_message "#{params.inspect}, #{action}, #{query}"
-end
 
-def respond_message message
-  content_type :json
-  {:text => message}.to_json
-end

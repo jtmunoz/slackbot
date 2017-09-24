eb27adde88b5bc0ee37fffbac275ffc22796a330
jordan <jordantmunoz@gmail.com>
2017-09-23 22:44:24 -0700

    adds gif view

--- app.rb
+++ app.rb
@@ -12,22 +12,29 @@ get '/gateway' do
 end
 
 post '/gateway' do
-  puts params
+  # puts params
   message = params[:text].gsub(params[:trigger_word], '').strip
-  puts message
+  # puts message
   message = message.split(' ').map {|c| c.strip.downcase }
-  puts message
+  # puts message
   action = message[0]
   message.shift
   query = message.join('+') 
   case action
     when 'trending'
-      giphy_url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=1"
+      giphy_url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=25"
+      # puts giphy_url
       resp = HTTParty.get(giphy_url)
+      # puts resp
       buffer = resp.body
-      result = JSON.parse(buffer)        
-      @src = respond_message result["data"][rand(0..25)]["url"]
-
+      # puts buffer
+      result = JSON.parse(buffer) 
+      puts "HERE  " * 10       
+      puts result
+      @src =  result["data"][rand(0..25)]["url"]["text"]
+      # @src = respond_message result["data"][rand(0..25)]["url"]
+      puts @src
+      haml :gif
     when 'search'
       giphy_url = "http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=dc6zaTOxFJmzC&limit=1"
       resp = HTTParty.get(giphy_url)
@@ -37,8 +44,8 @@ post '/gateway' do
   end
 end
 
-def respond_message message
-  content_type :json
-  {:text => message}.to_json
-end
+# def respond_message message
+#   content_type :json
+#   {:text => message}.to_json
+# end
       
